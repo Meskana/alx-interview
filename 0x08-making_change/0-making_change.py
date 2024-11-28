03aa25a271b398784b101c0648340f67d17e73f4
marcelinus <marcelinusilonuba21@gmail.com>
2024-11-28 19:26:45 +0100

    correction

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def makeChange(coins, total):
     """
     Determines the fewest number of coins needed to meet a given total.
@@ -12,7 +13,6 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    # Initialize a dp array with a large value, representing the minimum coins needed
     dp = [float('inf')] * (total + 1)
     dp[0] = 0  # Base case: 0 coins are needed to make a total of 0
 
